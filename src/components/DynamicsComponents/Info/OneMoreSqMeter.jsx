f68c95ab32e6f9addef87aa53c4c543fbbdf8ba7
Berlinchik <davidarts11@gmail.com>
2023-05-16 12:11:47 +0300

    Update OneMoreSqMeter.jsx

--- src/components/DynamicsComponents/Info/OneMoreSqMeter.jsx
+++ src/components/DynamicsComponents/Info/OneMoreSqMeter.jsx
@@ -1,4 +1,5 @@
 import image from 'assets/img/one-more-sq-meter.png';
+import image2x from 'assets/img/one-more-sq-meter-2x.png';
 import s from './OneMoreSqMeter.module.scss';
 import useCalcPrice from 'components/DynamicsComponents/hooks/useCalcPrice';
 
@@ -12,9 +13,16 @@ const OneMoreSqMeter = () => {
           To add more <span className={s.accent}>1 sq.m</span> for planning, it
           remains to accumulate
         </p>
-        <p className={s.sum}>{priceForOneMOreSqM + '₴'}</p>
+        <p className={s.sum}>{(priceForOneMOreSqM || 0) + '₴'}</p>
       </div>
-      <img className={s.img} src={image} alt="Finance app" width='185' height='165'/>
+      <img
+        className={s.img}
+        src={image}
+        srcSet={image2x}
+        alt="Finance app"
+        width="185"
+        height="165"
+      />
     </div>
   );
 };
